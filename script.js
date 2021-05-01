edeb1787b4f3018dc6921669d3bdf4cdb5d7f9ed
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2021-05-01 12:31:02 +0600

    working with submit button

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 const generateBtn = document.getElementById('generate');
 generateBtn.addEventListener('click', function(){
-    form = document.getElementById('form').value;
+    form = document.getElementById('newPin').value;
     for(i = 1000; i <= 9999; i++){
        var randomNum = Math.random() * 10000;
        var result = Math.round(randomNum);
     }
-    document.getElementById('form').value = result;
+    document.getElementById('newPin').value = result;
 })
+
+const displayNum = document.getElementById("display").value;
+
+function insert (number){
+    document.getElementById("display").value = document.getElementById("display").value + number;
+}
+
+function clearAll(){
+    document.getElementById("display").value = '';
+}
+
+function backSpace(){
+    var num = document.getElementById("display").value;
+    num = num.substring(0, num.length - 1);
+    document.getElementById("display").value = num;
+}
+
+
+function submitBtn(){
+    generatePin = document.getElementById('newPin').value;
+    matchPin = parseInt(document.getElementById('display').value);
+    if(generatePin == matchPin){
+        document.getElementById('matched').style.display = 'block';
+        document.getElementById('non-matched').style.display = 'none';
+    }
+    else{
+        document.getElementById('non-matched').style.display = 'block';
+        document.getElementById('matched').style.display = 'none';
+    }
+}
